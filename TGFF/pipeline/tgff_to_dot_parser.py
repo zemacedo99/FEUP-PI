b0a829b845b4d57464b8655863e16e8a387a07ae
Zé Macedo <up201705226@fe.up.pt>
2022-05-02 18:19:10 +0100

    Update tgff_to_dot_parser.py

--- TGFF/pipeline/tgff_to_dot_parser.py
+++ TGFF/pipeline/tgff_to_dot_parser.py
@@ -7,14 +7,14 @@ graph = None
 
 def add_task(task_details):
     task_dets=task_details.split()
-    print(task_dets)
+    #print(task_dets)
     #my_node = pydot.Node(task_dets[0],type = int(task_dets[2]),host = int(task_dets[4]))
     my_node = pydot.Node(task_dets[0],type = int(task_dets[2]))
     #create a new task with      :name       ,type          ,host
     graph.add_node(my_node)
 def add_arc(arc_details):
     arc_dets=arc_details.split()
-    print(arc_dets)
+    #print(arc_dets)
     my_edge = pydot.Edge(src=arc_dets[2],dst=arc_dets[4],name= arc_dets[0],type=arc_dets[6])
     #create a new arc with    :name       ,pe_from    ,pe_to      ,type
     graph.add_edge(my_edge)
@@ -39,29 +39,49 @@ def process_block(block):
 
     tg_name = None
     period = None
-    for line in block:
-        if line.startswith("@"):
-            tg_name = line.strip('@').strip('{')
-            graph = pydot.Dot(tg_name, graph_type='graph', bgcolor='yellow')
-            print(tg_name)
-        elif line.startswith("PERIOD"):
-            period = float(line.strip(' PERIOD'))
-            print(period)
-            break
-    for line in block:
-        if line.startswith("TASK"):
-            task = line.strip('TASK ')
-            add_task(task)
-        elif line.startswith("ARC"):
-            arc = line.strip('ARC ')
-            add_arc(arc)
-            #scenario.graphs[tg_name].add_arc(line.strip('ARC '))
-        elif line.startswith("SOFT_DEADLINE"):
-            temp = line.strip('SOFT_DEADLINE ')
-            print(temp)
-        elif line.startswith("HARD_DEADLINE"):
-            temp = line.strip('HARD_DEADLINE ')
-            print(temp)
+    
+    if "HYPERPERIOD" in block[0]:
+        hyperperiod= float(block[0].strip('@HYPERPERIOD '))
+    
+    elif "PE" in block[0]:
+        #TODO: Parse tables
+        core_name = None
+        i = 0
+        core_name= block[0].strip('@').strip('{')
+        print(core_name)
+        print(block[1].strip('#'))
+        print(block[2].strip('#'))
+        print(block[4].strip('#'))
+        #create table
+        for i in range(5, len(block)):
+            if not block[i].startswith('#'):
+                #add_row to table
+                tablerow = block[i]
+                print(tablerow)
+    else:
+        #Parse Tasks
+        for line in block:
+            if line.startswith("@"):
+                tg_name = line.strip('@').strip('{')
+                graph = pydot.Dot(tg_name, graph_type='graph')
+                #print(tg_name)
+            elif line.startswith("PERIOD"):
+                period = float(line.strip(' PERIOD'))
+                print(period)
+                break
+        for line in block:
+            if line.startswith("TASK"):
+                task = line.strip('TASK ')
+                add_task(task)
+            elif line.startswith("ARC"):
+                arc = line.strip('ARC ')
+                add_arc(arc)
+            elif line.startswith("SOFT_DEADLINE"):
+                temp = line.strip('SOFT_DEADLINE ')
+                print(temp)
+            elif line.startswith("HARD_DEADLINE"):
+                temp = line.strip('HARD_DEADLINE ')
+                print(temp)
 
 
 
@@ -73,29 +93,18 @@ def main():
     dot_file = file_name + ".dot"
     png_file = file_name + ".png"
     utils.clear()
-    print(tgff_file)
+    #print(tgff_file)
     
     
     global graph
-    #f = open(tgff_file, "r")
-    #line = f.readline()
+    
+    
+    
     with open(tgff_file) as input_file:
         for block in get_blocks(input_file):
             process_block(block)
             
-    #f.close()
-    
-    ### INPUT
-    #graph = pydot.Dot('my_graph', graph_type='graph', bgcolor='yellow')
 
-    # Add nodes
-    #my_node = pydot.Node('a', label='Foo')
-    #graph.add_node(my_node)
-
-    # Add edges
-    #my_edge = pydot.Edge('a', 'b', color='blue')
-    #graph.add_edge(my_edge)
-    
     ### OUTPUT
     
     # As a string:
@@ -107,10 +116,10 @@ def main():
     
     # Convert to a NetworkX graph:
     my_networkx_graph = networkx.drawing.nx_pydot.from_pydot(graph)
+
     
     dot_to_png = 'dot -Tpng ' + dot_file +' -o ' + png_file
     os.system(dot_to_png)
     
      
 main()
-     
